7019095ab2c5e70dcf9a0b0b5eae473092c00816
zhouwang <wang.zhou@breton.com>
2026-01-01 13:58:00 +0800

    modify compensation

--- src/compensation/src/compensation.cpp
+++ src/compensation/src/compensation.cpp
@@ -3,15 +3,13 @@
 #include "sensor_msgs/msg/nav_sat_fix.hpp"
 #include "novatel_oem7_msgs/msg/heading2.hpp"
 #include "novatel_oem7_msgs/msg/inspva.hpp"
+#include "novatel_oem7_msgs/msg/inspvax.hpp"
 #include "nav_msgs/msg/path.hpp"
 #include "novatel_oem7_msgs/msg/bestgnsspos.hpp"
 
 #include "rclcpp/qos.hpp"
 #include "rclcpp/executors.hpp"
 
-#include "gloc/initializer.h"
-#include "gloc/gps_processor.h"
-#include "gloc/imu_processor.h"
 #include "gloc/message.h"
 #include "gloc/gloc_data_buffer.h"
 #include "base_utils/coordinate.h"
@@ -22,10 +20,10 @@
 #include <fstream>
 
 
-class MotionDistortionNode : public rclcpp::Node
+class CompensationNode : public rclcpp::Node
 {
 public:
-  MotionDistortionNode() : Node("motion_distortion_node")
+  CompensationNode() : Node("compensation_node")
   {
     init();
   }
@@ -43,16 +41,16 @@ public:
     qos.keep_last(10);
 
     ins_sub_ = this->create_subscription<novatel_oem7_msgs::msg::INSPVA>(
-      "/bynav/inspva", qos, std::bind(&MotionDistortionNode::ins_callback, this, std::placeholders::_1));
+      "/bynav/inspvax", qos, std::bind(&CompensationNode::ins_callback, this, std::placeholders::_1));
 
     lidar_sub_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
-    "/iv_points", qos, std::bind(&MotionDistortionNode::ins_callback, this, std::placeholders::_1));
+    "/iv_points", qos, std::bind(&CompensationNode::ins_callback, this, std::placeholders::_1));
 
 
     return true;
   }
 
-  void ins_callback(const novatel_oem7_msgs::msg::INSPVA::SharedPtr msg)
+  void ins_callback(const novatel_oem7_msgs::msg::INSPVAX::SharedPtr msg)
   {
       Ins ins;
       ins._longitude = msg->longitude;
@@ -74,7 +72,7 @@ public:
       SingletonDataBuffer::getInstance()._ins_buffer.push(ins);
   }
 
-  rclcpp::Subscription<novatel_oem7_msgs::msg::INSPVA>::SharedPtr ins_sub_;
+  rclcpp::Subscription<novatel_oem7_msgs::msg::INSPVAX>::SharedPtr ins_sub_;
   rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr lidar_sub_;
 
 };
@@ -83,7 +81,7 @@ int main(int argc, char * argv[])
 {
     rclcpp::init(argc, argv);
     rclcpp::executors::MultiThreadedExecutor executor;
-    auto node = std::make_shared<MotionDistortionNode>();
+    auto node = std::make_shared<CompensationNode>();
     executor.add_node(node);
     executor.spin();
     rclcpp::shutdown();
